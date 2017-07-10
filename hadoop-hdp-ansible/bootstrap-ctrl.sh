22ad0eb1418403313f1fe5f08dc43d43fdf10949
Robert Jonczy <robert.jonczy@gmail.com>
2017-07-10 22:17:47 +0200

    temporary solution

--- hadoop-hdp-ansible/bootstrap-ctrl.sh
+++ hadoop-hdp-ansible/bootstrap-ctrl.sh
@@ -6,8 +6,8 @@ yum install -y ansible
 
 # configure hosts file for our internal network defined by Vagrantfile
 cat >> /etc/hosts <<EOL
-192.168.50.50   ctrl
-192.168.50.51   hdp1
-192.168.50.52   hdp2
-192.168.50.53   hdp3
+192.168.50.50   ctrl    ctrl.jonczy.lan
+192.168.50.51   hdp1    hdp1.jonczy.lan
+192.168.50.52   hdp2    hdp2.jonczy.lan
+192.168.50.53   hdp3    hdp3.jonczy.lan
 EOL
